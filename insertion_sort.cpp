0effe64ddec54a22c43653e84e5e9aa3abcedc99
NekoKaicho <adecta7@gmail.com>
2026-03-12 10:53:52 +0700

    membuat perulangan untuk input element array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -20,3 +20,8 @@ void input (){      //procedure input
     cout << "============================================";
     cout << "==========Masukan Element Array=============";
     cout << "============================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
