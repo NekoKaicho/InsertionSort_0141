781044b7eae268950d6a95fe035230a34050027a
NekoKaicho <adecta7@gmail.com>
2026-03-12 10:47:12 +0700

    menambah tampilan header untuk masukkan element array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -16,3 +16,7 @@ void input (){      //procedure input
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
     }
+    cout << endl;
+    cout << "============================================";
+    cout << "==========Masukan Element Array=============";
+    cout << "============================================";
