66c4d899f6061f6b17cededd1adec7b0bddd9f5a
NekoKaicho <adecta7@gmail.com>
2026-03-12 11:08:31 +0700

    tambah perulangan untuk menampilkan semua elemen array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -55,4 +55,11 @@ void display(){
     cout << "\nElement Array" << endl;
     cout << "=======================";
 
+     for (int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+
 
